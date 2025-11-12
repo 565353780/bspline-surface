0e02b9a22bb5d8e56cfa123d88f11fd970b63def
chli <chli@chlideMacBook-Pro.local>
2025-11-12 19:01:55 +0800

    add setup bash files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# bspline-surface
+# Bspline Surface
+
+## Setup
+
+```bash
+conda create -n bs python=3.10
+conda activate bs
+./setup.sh
+```
+
+## Run
+
+```bash
+python demo.py
+```
+
+## Enjoy it~
---
+++ dev_setup.sh
@@ -0,0 +1,12 @@
+if [ "$(uname)" = "Darwin" ]; then
+  brew install bear
+elif [ "$(uname)" = "Linux" ]; then
+  sudo apt install bear -y
+fi
+
+pip install -U ninja open3d
+
+pip install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu124
+
+./compile.sh
