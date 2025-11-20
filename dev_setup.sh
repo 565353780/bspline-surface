fb7e14ece4558d11f66dbcc1e6a3fc308286c1f6
chli <chli@chlideMacBook-Pro.local>
2025-11-20 19:28:47 +0800

    remove bear install for server

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,8 +1,8 @@
-if [ "$(uname)" = "Darwin" ]; then
-  brew install bear
-elif [ "$(uname)" = "Linux" ]; then
-  sudo apt install bear -y
-fi
+#if [ "$(uname)" = "Darwin" ]; then
+#  brew install bear
+#elif [ "$(uname)" = "Linux" ]; then
+#  sudo apt install bear -y
+#fi
 
 pip install -U ninja open3d
 
